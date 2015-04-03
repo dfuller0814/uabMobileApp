397579ee27804a0166d144fb5e91ef7db5931a93
Daniel Fuller <danielfuller@Daniels-MacBook-Pro.local>
2015-04-03 13:17:30 -0400

    Changes to build page for Assignment 4

--- Assignment 4/JS/Assignment4.js
+++ Assignment 4/JS/Assignment4.js
@@ -1,25 +1,91 @@
-function toggleAreas(){
-    var dropDownListOption = document.getElementById("areaDropdown").selectedIndex;
+$(document).ready(function(){
+    $("footer").animate({width: "toggle"},"slow");
+    $("#areaDropdown").change(function() {
+        var selectedIndex = $("#areaDropdown option:selected").index();
+        
+        if (selectedIndex == "0") {
+            if ($("#area1Wrapper").css("display") != "none"){
+                $("#area1Wrapper").fadeOut("fast");
+            }
+            else if ($("#area2Wrapper").css("display") != "none") {
+                $("#area2Wrapper").fadeOut("fast");
+            }    
+        }
+        
+        else if(selectedIndex == "1"){
+            if ($("#area2Wrapper").css("display") != "none"){
+                $("#area2Wrapper").fadeOut("fast",function(){
+                    $("#area1").show();
+                    $("#area1Wrapper").fadeIn("fast").css({"display": "inline-block"});
+                });
+            }
+            else{
+                $("#area1").show();
+                $("#area1Wrapper").fadeIn("fast").css({"display": "inline-block"});
+            }
+        }
+        
+        else if(selectedIndex == "2"){
+            if ($("#area1Wrapper").css("display") != "none"){
+                 $("#area1Wrapper").fadeOut("fast",function(){
+                    $("#area2").show();
+                    $("#area2Wrapper").fadeIn("fast").css({"display": "inline-block"});
+                 });
+            }
+            else{
+                $("#area2").show();
+                $("#area2Wrapper").fadeIn("fast").css({"display": "inline-block"});
+            }
+        }
+    });
+});
+/*    
+    var areasWrapper = document.getElementById("areasWrapper");
     var area1 = document.getElementById("area1");
     var area2 = document.getElementById("area2");
-    var area1Vis = window.getComputedStyle(area1,null).getPropertyValue("visibility");
-    var area2Vis = window.getComputedStyle(area2,null).getPropertyValue("visibility");
+    var area1Vis = window.getComputedStyle(area1,null).getPropertyValue("display");
+    var area2Vis = window.getComputedStyle(area2,null).getPropertyValue("display");
     
     /* Test selected index of the dropdown list to determine which section toggle. This is to make the logic more dynamic in case the
      * dropdown option names were to change.
-     * Index 0 = Area 1
-     * Index 1 = Area 2, etc. */
+     * Index 0 = None
+     * Index 1 = Area 1
+     * Index 2 = Area 2, etc. 
     
     if (dropDownListOption == "0") {
-        if (area2Vis == "visible") {
-            area2.style.visibility = "hidden";
-        }
-        else if (area1Vis == "hidden") {
-            area1.style.visibility = "visible";
+        if ((area1Vis != "none") || (area2Vis != "none")) {
+            $("areasWrapper").fadeOut("fast", function(){
+                $(area1).toggle(false);
+                $(area2).toggle(false)
+            });
         }
     }
     
-    if (dropDownListOption == "1") {
-        area2.innerHTML = "Area 2";
+    else if (dropDownListOption == "1") {
+        if (area2Vis != "none") {
+            $("areasWrapper").fadeOut("fast", function(){
+                $(area2).toggle(false);
+            });
+        }
+        if (area1Vis == "none") {
+            $(area1).toggle(true);
+            $("#areasWrapper").fadeIn("fast").css({"display":"inline-block"});
+            var newArea1Dis = window.getComputedStyle(area1,null).getPropertyValue("display");
+        }
     }
+    
+    else if (dropDownListOption == "2") { 
+        if (area1Vis != "none") {
+            $(area1).toggle(false);  
+        }
+        /*
+        if (area2Vis == "none") {
+            $(area2).toggle(true);
+            $("#areasWrapper").fadeIn("fast").css({"display":"inline-block"});
+              
+        }
+        
+    }
+    
 }
+*/
