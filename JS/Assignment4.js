31d55024ce4cede1b3367602285c098fd8fb0af7
Daniel Fuller <danielfuller@Daniels-MacBook-Pro.local>
2015-04-04 16:10:30 -0400

    Edit show method

--- JS/Assignment4.js
+++ JS/Assignment4.js
@@ -4,36 +4,36 @@ $(document).ready(function(){
         
         if (selectedIndex == "0") {
             if ($("#area1Wrapper").css("display") != "none"){
-                $("#area1Wrapper").fadeOut();
+                $("#area1Wrapper").hide();
             }
             else if ($("#area2Wrapper").css("display") != "none") {
-                $("#area2Wrapper").fadeOut();
+                $("#area2Wrapper").hide();
             }    
         }
         
         else if(selectedIndex == "1"){
             if ($("#area2Wrapper").css("display") != "none"){
-                $("#area2Wrapper").fadeOut(function(){
+                $("#area2Wrapper").hide(function(){
                     $("#area1").show();
-                    $("#area1Wrapper").fadeIn();
+                    $("#area1Wrapper").show();
                 });
             }
             else{
                 $("#area1").show();
-                $("#area1Wrapper").fadeIn();
+                $("#area1Wrapper").show();
             }
         }
         
         else if(selectedIndex == "2"){
             if ($("#area1Wrapper").css("display") != "none"){
-                 $("#area1Wrapper").fadeOut(function(){
+                 $("#area1Wrapper").hide(function(){
                     $("#area2").show();
-                    $("#area2Wrapper").fadeIn();
+                    $("#area2Wrapper").show();
                  });
             }
             else{
                 $("#area2").show();
-                $("#area2Wrapper").fadeIn();
+                $("#area2Wrapper").show();
             }
         }
     });
